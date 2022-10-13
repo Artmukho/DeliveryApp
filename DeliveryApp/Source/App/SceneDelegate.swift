10a896ca25c021860f75d2feb8668d6ff974e049
Artem Muho <artem.muho@yandex.ru>
2022-10-13 14:34:10 +0300

    Initial commit.

---
+++ DeliveryApp/Source/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  DeliveryApp
+//
+//  Created by Artem Muho on 13.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+   var window: UIWindow?
+
+   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+      guard let windowScene = (scene as? UIWindowScene) else { return }
+      window = UIWindow(windowScene: windowScene)
+      let mainScreen = UINavigationController(rootViewController: ViewController())
+      window?.rootViewController = mainScreen
+      window?.makeKeyAndVisible()
+   }
+}
+
